8fa451ae2983f150d213ccf047cbfa5210ab002e
FranchiniMae <fmae.mercado@gmail.com>
2016-03-26 22:17:11 -0700

    Adds body parser and testing commit

--- server.js
+++ server.js
@@ -18,13 +18,18 @@ app.use(express.static(__dirname + '/public'));
 // set view engine to hbs (handlebars)
 // app.set('view engine', 'hbs');
 
+// parse form data ( application/x-www-form-urlencoded )
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());  // ADD THIS LINE
+
+
 // connect to mongodb
 mongoose.connect('mongodb://localhost/ice_cream');
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, './views', 'index.html'));
 });
-
 
 // listen on port 3000
 app.listen(3000, function() {
