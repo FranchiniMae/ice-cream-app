d26f42686649b31053b2387bb694f7223879a780
FranchiniMae <fmae.mercado@gmail.com>
2016-03-26 22:08:39 -0700

    Adds working connection to index.html

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
+    path = require('path'),
     mongoose = require('mongoose');
 
 // configure bodyParser (for receiving form data)
@@ -18,7 +19,12 @@ app.use(express.static(__dirname + '/public'));
 // app.set('view engine', 'hbs');
 
 // connect to mongodb
-mongoose.connect('mongodb://localhost/mean_sample');
+mongoose.connect('mongodb://localhost/ice_cream');
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './views', 'index.html'));
+});
+
 
 // listen on port 3000
 app.listen(3000, function() {
