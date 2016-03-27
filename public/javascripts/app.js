630a2bef2c4015d687825d786a32632a99790146
FranchiniMae <fmae.mercado@gmail.com>
2016-03-26 22:15:56 -0700

    Adds workign home.html connection

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,2 +1,32 @@
 console.log('app.js loaded!');
 var app = angular.module('iceCreamApp', ['ui.router']);
+
+app.config(config);
+
+config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+function config($stateProvider, $urlRouterProvider, $locationProvider) {
+    console.log('config');
+    //this allows us to use routes without hash params!
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+    // for any unmatched URL redirect to /
+    $urlRouterProvider.otherwise("/");
+
+     $stateProvider
+      .state('home', {
+        url: "/",
+        controller: 'HomeController',
+        controllerAs: 'home',
+        templateUrl: "templates/home.html"
+        // template: 'Home!'
+      });
+  }
+
+  app.controller('HomeController', HomeController);
+
+function HomeController() {
+  var vm = this;
+  vm.homeTest = "Welcome to the homepage!";
+}
